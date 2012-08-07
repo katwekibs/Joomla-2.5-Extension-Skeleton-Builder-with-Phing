4c519085fc943027181d1ef35abeedbf0945475c
Popovics András <popovics@ndras.hu>
2012-08-07 02:42:01 +0200

    Added install/uninstall/update script and bugfixing the joomlabuilder's copy_all target

---
+++ skeleton/com_component/script.php
@@ -0,0 +1,67 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * Script file of @@componentname@@ component
+ */
+class com_@@componentname@@InstallerScript
+{
+	/**
+	 * method to install the component
+	 *
+	 * @return void
+	 */
+	function install($parent) 
+	{
+		// $parent is the class calling this method
+		//$parent->getParent()->setRedirectURL('index.php?option=@@com_component@@');
+		echo '<p>' . JText::_('@@com_component_uppercase@@_INSTALL_TEXT') . '</p>';
+	}
+ 
+	/**
+	 * method to uninstall the component
+	 *
+	 * @return void
+	 */
+	function uninstall($parent) 
+	{
+		// $parent is the class calling this method
+		echo '<p>' . JText::_('@@com_component_uppercase@@_UNINSTALL_TEXT') . '</p>';
+	}
+ 
+	/**
+	 * method to update the component
+	 *
+	 * @return void
+	 */
+	function update($parent) 
+	{
+		// $parent is the class calling this method
+		echo '<p>' . JText::sprintf('@@com_component_uppercase@@_UPDATE_TEXT', $parent->get('manifest')->version) . '</p>';
+	}
+ 
+	/**
+	 * method to run before an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function preflight($type, $parent) 
+	{
+		// $parent is the class calling this method
+		// $type is the type of change (install, update or discover_install)
+		echo '<p>' . JText::_('@@com_component_uppercase@@_PREFLIGHT_' . $type . '_TEXT') . '</p>';
+	}
+ 
+	/**
+	 * method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function postflight($type, $parent) 
+	{
+		// $parent is the class calling this method
+		// $type is the type of change (install, update or discover_install)
+		echo '<p>' . JText::_('@@com_component_uppercase@@_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
+	}
+}
